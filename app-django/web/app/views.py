7a80bd9cf4ddf226fbdc7df13d034786492f688d
Valdir Silva <valdipiresba@gmail.com>
2023-05-26 18:18:37 -0300

    Criando e listando usuarios

--- app-django/web/app/views.py
+++ app-django/web/app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserForm
+from .models import User
 
-# Create your views here.
+def index(request):
+    # Pega todos os uauários do banco
+    users = User.objects.all()
+
+    context = {
+       'users': users 
+    }
+
+    return render(request, 'index.html', context )
+
+def create(request):
+    # Verifica se o metodo é GET  
+
+    if request.method == 'GET':
+        form = UserForm() # Pega o formulário vazio
+
+        # criando um contexto formulário
+        context = {
+            'form': form,
+        }
+        
+            # Enviando o form para a view 
+        return render(request, 'criar.html', context=context)    
+    else:
+        form = UserForm(request.POST)
+        if form.is_valid():
+            
+            form.save() 
+            return redirect(index)    
+
+
+def modify(request, user_id):
+    print(user_id)
+    return render(request, 'user/index.html')
